d9aa9710b8fb3c828c21db813f4b27b68647af28
moack <1068045+moack@users.noreply.github.com>
2026-02-03 13:07:50 +0100

    Updated Jest configuration for pipeline

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,22 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/tests/**/*.spec.ts', '**/tests/**/*.test.ts'],
-    transform: {
-      '^.+\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
-    },
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.spec.ts', '**/tests/**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+  },
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: 'test-reports',
+        outputName: 'junit.xml',
+      },
+    ],
+  ],
 };
 
 export default config;
